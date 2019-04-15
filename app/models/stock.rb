f4ebf8d93741ed980b8b067b92140fff7010a374
ozy <ozyoz614@yahoo.com>
2019-04-14 23:59:23 -0400

    add stock tracking limits

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,20 +1,25 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
 
   def self.new_from_lookup(ticker_symbol)
     begin
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+
+
+      price = strip_commas(looked_up_stock.l)
+      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
     rescue Exception => e
       return nil
     end
-    end
-
-
+  end
 
   def self.strip_commas(number)
     number.gsub(",", "")
   end
 
 end
-
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
+
+
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+
+  end
 end
